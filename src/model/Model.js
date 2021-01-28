33ac5d9513f7a51d7e60399ee815503c18874c83
unknown <muhammadyusuff1312@gmail.com>
2021-01-28 15:58:41 +0700

    first commit

---
+++ src/model/Model.js
@@ -0,0 +1,20 @@
+
+
+export const data = JSON.parse(`[{
+	"type": "radio",
+	"soal": "Siapakah penemu baling-baling bambu",
+	"a": "Doraemon",
+	"b": "hanif",
+	"c": "nobita",
+	"d": "naruto",
+	"jawabannya": "a"
+}, {
+	"type": "checkbox",
+	"soal": "pilihlah 3 yang benar mengenai aqil",
+	"option": ["Trainer Juara Coding", "Manusia", "Anggota Avenger"],
+	"jawabannya": ["Manusia", "Anggota Avenger"]
+}, {
+	"type": "essay",
+	"soal": "Ceritakan pengalaman anda ketika sedang tidur",
+	"jawabannya": 10
+}]`);
